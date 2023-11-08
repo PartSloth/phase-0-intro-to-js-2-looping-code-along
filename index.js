4414c046e5b48d831140a9168685d03490483a86
PartSloth <louiza.mak@gmail.com>
2023-11-08 10:39:28 -0600

    Created for and while functions.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const newArray = [];
+
+function writeCards(array, event) {
+    for (let i=0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(i) {
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+}
